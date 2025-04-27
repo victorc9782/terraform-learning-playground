1c581679ddf48edab98155af877f4191cce61b92
Victor.C <victorchu2002@gmail.com>
2025-04-27 16:42:10 +0100

    Add security group rule

--- main.tf
+++ main.tf
@@ -14,9 +14,29 @@ provider "aws" {
   profile = "default"
 }
 
+resource "aws_security_group" "default_ec2" {
+  name        = "default_ec2"
+  description = "Security group with no inbound traffic"
+
+  # No inbound rules defined - all inbound traffic blocked
+
+  # Allow all outbound traffic (this is the default)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "default-ec2"
+  }
+}
+
 resource "aws_instance" "example_server" {
   ami           = "ami-0ce8c2b29fcc8a346"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.default_ec2.id]
 
   tags = {
     Name = "TerraformExampleServer"
